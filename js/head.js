9564d32ba8cd2a030b4de1e25a6d48325804ea73
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-02-14 21:26:20 -0500

    Create head.js

---
+++ js/head.js
@@ -0,0 +1,8 @@
+let headCode = 
+    '<head>\n' +
+    '<title>\n' +
+    'Kat Cottrell | Home\n' +
+    '</title>\n' +
+    '<link rel="icon" href="images/kc_fav.png">\n' +
+    '</head>';
+document.write(headCode);
