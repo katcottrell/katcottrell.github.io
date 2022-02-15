094bfa99269fb3c7f40137be700ef3fb32ab0304
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-02-14 23:05:30 -0500

    Wrote makeHeader fx

--- js/header.js
+++ js/header.js
@@ -1,18 +1,23 @@
-let headerCode = 
-    '<header>\n' +
-		'<link rel="stylesheet" href="style.css">\n' +
-		'<div id="header-image-menu">\n' +
-		'<img src="images/My_Banner.png">\n' +
-		'</div>\n' +
-		'<div class="topnav">\n' +
-		'<code><a href="index.html">Kat Cottrell</a></code>\n' +
-		'<a href="index.html">Home</a>\n' +
-		'<a class="active" href="about.html">About</a>\n' +
-		'<a href="resume.html">Resume</a>\n' +
-		'<a href="tech.html">Technical Skills</a>\n' +
-		'<a href="projects.html">Projects</a> \n' +
-		'<a href="art.html">Art</a> \n' +
-		'<a href="contact.html">Contact</a>\n' +
-		'</div>\n' +
-	  '</header>';
-document.write(headerCode);
+function makeHeader(tab) {
+	let home     = tab === "home"     ? '<a class="active" href="index.html">Home</a>\n'            : '<a href="index.html">Home</a>\n';
+	let about    = tab === "about"    ? '<a class="active" href="about.html">About</a>\n'           : '<a href="about.html">About</a>\n';
+	let resume   = tab === "resume"   ? '<a class="active" href="resume.html">Resume</a>\n'         : '<a href="resume.html">Resume</a>\n';
+	let tech     = tab === "tech"     ? '<a class="active" href="tech.html">Technical Skills</a>\n' : '<a href="tech.html">Technical Skills</a>\n';
+	let projects = tab === "projects" ? '<a class="active" href="projects.html">Projects</a>\n'     : '<a href="projects.html">Projects</a>\n';
+	let art      = tab === "art"      ? '<a class="active" href="art.html">Art</a>\n'               : '<a href="art.html">Art</a>\n';
+	let contact  = tab === "contact"  ? '<a class="active" href="contact.html">Contact</a>\n'       : '<a href="contact.html">Contact</a>\n';
+	
+	let headerCode = 
+	    '<header>\n' +
+	    '<link rel="stylesheet" href="style.css">\n' +
+	    '<div id="header-image-menu">\n' +
+	    '<img src="images/My_Banner.png">\n' +
+	    '</div>\n' +
+	    '<div class="topnav">\n' +
+	    '<code><a href="index.html">Kat Cottrell</a></code>\n' +
+	    home + about + resume + tech + projects + art + contact +
+	    '</div>\n' +
+	    '</header>';
+	
+	document.write(headerCode);
+}
