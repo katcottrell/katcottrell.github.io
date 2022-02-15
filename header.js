23aa20110c9e3c8f9aa4a53a97ffdf36016f06f3
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-02-14 21:17:51 -0500

    Create header.js

---
+++ header.js
@@ -0,0 +1,18 @@
+let headerCode = 
+    '<header>\n' +
+		'<link rel="stylesheet" href="style.css">\n' +
+		'<div id="header-image-menu">\n' +
+		'<img src="My_Banner.png">\n' +
+		'</div>\n' +
+		'<div class="topnav">\n' +
+		'<code><a href="index.html">Kat Cottrell</a></code>\n' +
+		'<a href="index.html">Home</a>\n' +
+		'<a class="active" href="about.html">About</a>\n' +
+		'<a href="resume.html">Resume</a>\n' +
+		'<a href="tech.html">Technical Skills</a>\n' +
+		'<a href="projects.html">Projects</a> \n' +
+		'<a href="art.html">Art</a> \n' +
+		'<a href="contact.html">Contact</a>\n' +
+		'</div>\n' +
+	  '</header>';
+document.write(headerCode);
