ad82e0bbf6a88953988ee979e328d4408dc5f012
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-03-16 01:29:13 -0400

    fixed double class issue

--- js/sidenav.js
+++ js/sidenav.js
@@ -8,18 +8,20 @@ let abs                = 'https://katcottrell.github.io/tech/';
 function makeSideNav(tab) {  
 	let sideNavCode = '<div class="sidenav">\n<a class="cats">Languages</a>\n';
 	for (let i = 0; i < languageLinks.length; i++) {
-		sideNavCode += '<a class="tabs" ';
 		if (tab === languageLinks[i]) {
-			sideNavCode += 'class="active" ';
+			sideNavCode += '<a class="active" ';
+		} else {
+			sideNavCode += '<a class="tabs" ';
 		}
 		sideNavCode += 'href="' + abs + languageLinks[i] + '-skills.html">' + 
 				languageTitles[i] + '</a>\n';
 	}
 	sideNavCode += '<a class="cats">Other Skills</a>\n';
 	for (let i = 0; i < otherSkillLinks.length; i++) {
-		sideNavCode += '<a class="tabs" ';
 		if (tab === otherSkillLinks[i]) {
-			sideNavCode += 'class="active" ';
+			sideNavCode += '<a class="active" ';
+		} else {
+			sideNavCode += '<a class="tabs" ';
 		}
 		sideNavCode += 'href="' + abs + otherSkillLinks[i] + '-skills.html">' + 
 				otherSkillTitles[i] + '</a>\n';
