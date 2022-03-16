a7231c8eb9229600f43b224b5bd11d1e1170f6d1
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-03-16 00:44:45 -0400

    Rewrote sideNavCode

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,37 +1,29 @@
-function makeSideNav(tab) {
-	let abs    = 'https://katcottrell.github.io/';
-	let tech   = tab === "tech"   ? 'class="active" ' : '';
-	let c      = tab === "c"      ? 'class="active" ' : '';
-	let excel  = tab === "excel"  ? 'class="active" ' : '';
-	let git    = tab === "git"    ? 'class="active" ' : '';
-	let html   = tab === "html"   ? 'class="active" ' : '';
-	let java   = tab === "java"   ? 'class="active" ' : '';
-	let linux  = tab === "linux"  ? 'class="active" ' : '';
-	let ml     = tab === "ml"     ? 'class="active" ' : '';
-	let matlab = tab === "matlab" ? 'class="active" ' : '';
-	let python = tab === "python" ? 'class="active" ' : '';
-	let r      = tab === "r"      ? 'class="active" ' : '';
-	let db     = tab === "db"     ? 'class="active" ' : '';
-	let stata  = tab === "stata"  ? 'class="active" ' : '';
-	let dv     = tab === "dv"     ? 'class="active" ' : '';
-  
-	let sideNavCode = 
-	    '<div class="sidenav">\n' + 
-	    '<a ' + tech   + 'href="' + abs + 'tech.html" style="font-weight:bold;font-size:20px;">My Technical Skills</a>\n' +
-	    '<a ' + c      + 'href="' + abs + 'tech/c-skills.html">C</a>\n' +
-	    '<a ' + excel  + 'href="' + abs + 'tech/excel-openrefine-skills.html">Spreadsheets</a>\n' +
-	    '<a ' + git    + 'href="' + abs + 'tech/git-skills.html">Git & Github</a>\n' +
-	    '<a ' + html   + 'href="' + abs + 'tech/html-css-skills.html">HTML & CSS</a>\n' +
-	    '<a ' + java   + 'href="' + abs + 'tech/java-skills.html">Java</a>\n' +
-	    '<a ' + linux  + 'href="' + abs + 'tech/linux-unix-skills.html">Linux & Unix</a>\n' +
-	    '<a ' + ml     + 'href="' + abs + 'tech/ml-skills.html">Machine Learning</a>\n' +
-	    '<a ' + matlab + 'href="' + abs + 'tech/matlab-skills.html">MATLAB</a>\n' +
-	    '<a ' + python + 'href="' + abs + 'tech/python-skills.html">Python</a>\n' +
-	    '<a ' + r      + 'href="' + abs + 'tech/r-skills.html">R</a>\n' +
-	    '<a ' + db     + 'href="' + abs + 'tech/sql-skills.html">Databases</a>\n' +
-	    '<a ' + stata  + 'href="' + abs + 'tech/stata-skills.html">Stata</a>\n' +
-	    '<a ' + dv     + 'href="' + abs + 'tech/tableau-skills.html">Data Visualization</a>\n' +
-	    '</div>\n';
-	
+const languageLinks    = ['c', 'java', 'matlab', 'python', 'r', 'scheme'];
+const languageTitles   = ['C, C++, C#', 'Java', 'MATLAB', 'Python', 'R', 'Scheme (Lisp)'];
+const otherSkillLinks  = ['datavis', 'database', 'git', 'ml', 'spreadsheet', 'web', 'more'];
+const otherSkillTitles = ['Data Visualization', 'Databases', 'Git', 'Machine Learning', 
+			  'Spreadsheets', 'Web Development', 'More']
+let abs                = 'https://katcottrell.github.io/tech/';
+
+function makeSideNav(tab) {  
+	let sideNavCode = '<div class="sidenav">\n<a1>Languages</a1>\n';
+	for (let i = 0; i < languageLinks.length; i++) {
+		sideNavCode += '<a2 ';
+		if (tab === languageLinks[i]) {
+			sideNavCode += 'class="active" ';
+		}
+		sideNavCode += 'href="' + abs + languageLinks[i] + '-skills.html">' + 
+				languageTitles[i] + '</a2>\n';
+	}
+	sideNavCode += '<a1>Other Skills</a1>\n';
+	for (let i = 0; i < otherSkillLinks.length; i++) {
+		sideNavCode += '<a2 ';
+		if (tab === otherSkillLinks[i]) {
+			sideNavCode += 'class="active" ';
+		}
+		sideNavCode += 'href="' + abs + otherSkillLinks[i] + '-skills.html">' + 
+				otherSkillTitles[i] + '</a2>\n';
+	}
+	sideNavCode += '</div>\n';
 	document.write(sideNavCode);
 }
