f7560de5a59b93b79bc7d346eb0197cd5127a40d
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-03-31 21:16:37 -0400

    added hr

--- js/sidenav.js
+++ js/sidenav.js
@@ -8,6 +8,7 @@ let abs = 'https://katcottrell.github.io/';
 function makeSideNav(tab) {  
 	let sideNavCode = '\n<div class="sidenav">\n';
 	sideNavCode    += '<a class="sideTitle" href="' + abs + 'tech.html">My Tech Skills</a>\n';
+	sideNavCode    += '<hr style="width:90%;">\n';
 	sideNavCode    += '<a class="sideHeading">Programming Languages</a>\n';
 	for (let i = 0; i < languageLinks.length; i++) {
 		if (tab === languageLinks[i]) {
