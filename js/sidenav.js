a2580a3059e76f8361de111e56eeb2d14239f988
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-03-31 21:03:55 -0400

    Added title and reformatted

--- js/sidenav.js
+++ js/sidenav.js
@@ -3,27 +3,29 @@ const languageTitles   = ['C, C++, C#', 'Java', 'MATLAB', 'Python', 'R', 'Scheme
 const otherSkillLinks  = ['datavis', 'database', 'git', 'ml', 'spreadsheet', 'web', 'more'];
 const otherSkillTitles = ['Data Visualization', 'Databases', 'Git', 'Machine Learning', 
 			  'Spreadsheets', 'Web Development', 'More']
-let abs                = 'https://katcottrell.github.io/tech/';
+let abs = 'https://katcottrell.github.io/';
 
 function makeSideNav(tab) {  
-	let sideNavCode = '<div class="sidenav">\n<a class="cats">Programming Languages</a>\n';
+	let sideNavCode = '\n<div class="sidenav">\n';
+	sideNavCode    += '<a class="sideTitle" href="' + abs + 'tech.html">My Tech Skills</a>\n';
+	sideNavCode    += '<a class="sideHeading">Programming Languages</a>\n';
 	for (let i = 0; i < languageLinks.length; i++) {
 		if (tab === languageLinks[i]) {
 			sideNavCode += '<a class="active" ';
 		} else {
-			sideNavCode += '<a class="tabs" ';
+			sideNavCode += '<a class="sideTabs" ';
 		}
-		sideNavCode += 'href="' + abs + languageLinks[i] + '-skills.html">' + 
+		sideNavCode += 'href="' + abs + 'tech/' + languageLinks[i] + '-skills.html">' + 
 				languageTitles[i] + '</a>\n';
 	}
-	sideNavCode += '<a class="cats" style="margin-top:20px;">Other Skills</a>\n';
+	sideNavCode += '<a class="sideHeading" style="margin-top:20px;">Other Skills</a>\n';
 	for (let i = 0; i < otherSkillLinks.length; i++) {
 		if (tab === otherSkillLinks[i]) {
 			sideNavCode += '<a class="active" ';
 		} else {
-			sideNavCode += '<a class="tabs" ';
+			sideNavCode += '<a class="sideTabs" ';
 		}
-		sideNavCode += 'href="' + abs + otherSkillLinks[i] + '-skills.html">' + 
+		sideNavCode += 'href="' + abs + 'tech/' + otherSkillLinks[i] + '-skills.html">' + 
 				otherSkillTitles[i] + '</a>\n';
 	}
 	sideNavCode += '</div>\n';
