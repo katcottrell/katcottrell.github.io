53381b798c018eef79ec73adfd57fbbc59ee2b0e
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-04-04 19:16:30 -0400

    Code to make footer

---
+++ js/footer.js
@@ -0,0 +1,12 @@
+function makeFooter() {
+    let footerCode =                                                                            '\n' +
+        '<footer>'                                                                            + '\n' +
+        '<p class="footer-text"> Website by Kat Cottrell | '                                  + '\n' +
+        '<img src="images/mailIcon.png"       class="footer-icon" alt="Email me!">'           + '\n' +
+        '<img src="images/phoneIcon.png"      class="footer-icon" alt="Call me!">'            + '\n' +
+				'<img src="images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">' + '\n' +
+				'<img src="images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'   + '\n' +
+        '</p>'                                                                                + '\n' +
+        '</footer>'                                                                           + '\n';
+    document.write(footerCode);
+}
