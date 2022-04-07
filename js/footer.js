e5e68b80a16d722415343df857f6f91b36f4c1ec
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-04-07 00:18:36 -0400

    made paths absolute

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,13 @@
 function makeFooter() {
-    let footerCode =                                                                            '\n' +
-        '<footer>'                                                                            + '\n' +
-        '<p class="footer-text"> Website by Kat Cottrell | '                                  + '\n' +
-        '<img src="images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'           + '\n' +
-        '<img src="images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'            + '\n' +
-	'<img src="images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">' + '\n' +
-	'<img src="images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'   + '\n' +
-        '</p>'                                                                                + '\n' +
-        '</footer>'                                                                           + '\n';
-    document.write(footerCode);
+	let abs = 'https://katcottrell.github.io/';
+	let footerCode =                                                                                       '\n' +
+	    '<footer>'                                                                                       + '\n' +
+	    '<p class="footer-text"> Website by Kat Cottrell | '                                             + '\n' +
+	    '<img src="' + abs + 'images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'           + '\n' +
+	    '<img src="' + abs + 'images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'            + '\n' +
+	    '<img src="' + abs + 'images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">' + '\n' +
+	    '<img src="' + abs + 'images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'   + '\n' +
+	    '</p>'                                                                                           + '\n' +
+	    '</footer>'                                                                                      + '\n';
+	document.write(footerCode);
 }
