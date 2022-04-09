c8265fe48e4b3f9ffd12dde3d5cc2700a4d8d0cc
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-04-09 04:52:55 -0400

    added styling and open links in new tab with security

--- js/footer.js
+++ js/footer.js
@@ -1,22 +1,23 @@
 function makeFooter() {
 	let abs = 'https://katcottrell.github.io/';
+	let linkAttributes = 'target="_blank" rel="noopener noreferrer" style="color:transparent;"';
 	let footerCode =											  '\n' +
 	    '<footer>'												+ '\n' +
 	    '<p class="footer-text"> Website by Kat Cottrell | '						+ '\n' +
 	    
-	    '<a href="mailto:cottrellkat@gmail.com">'								+ '\n' +
+	    '<a href="mailto:cottrellkat@gmail.com" ' + linkAttributes + '>'					+ '\n' +
 	    '<img src="' + abs + 'images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'		+ '\n' +
 	    '</a>'												+ '\n' +
 	    
-	    '<a href="tel:7274531850">'										+ '\n' +
+	    '<a href="tel:7274531850" ' + linkAttributes + '>'							+ '\n' +
 	    '<img src="' + abs + 'images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'		+ '\n' +
 	    '</a>'												+ '\n' +
 	    
-	    '<a href="https://www.linkedin.com/in/katherine-cottrell-656490168/">'				+ '\n' +
+	    '<a href="https://www.linkedin.com/in/katherine-cottrell-656490168/" ' + linkAttributes + '>'	+ '\n' +
 	    '<img src="' + abs + 'images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">'	+ '\n' +
 	    '</a>'												+ '\n' +
 	    
-	    '<a href="https://github.com/katcottrell/">'							+ '\n' +
+	    '<a href="https://github.com/katcottrell/" ' + linkAttributes + '>'					+ '\n' +
 	    '<img src="' + abs + 'images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'	+ '\n' +
 	    '</a>'												+ '\n' +
 	    
