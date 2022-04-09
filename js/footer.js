9517e4f7390cd73153e16e94afdb40dcd49dbe27
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2022-04-09 04:40:51 -0400

    added links to footer

--- js/footer.js
+++ js/footer.js
@@ -1,13 +1,26 @@
 function makeFooter() {
 	let abs = 'https://katcottrell.github.io/';
-	let footerCode =                                                                                       '\n' +
-	    '<footer>'                                                                                       + '\n' +
-	    '<p class="footer-text"> Website by Kat Cottrell | '                                             + '\n' +
-	    '<img src="' + abs + 'images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'           + '\n' +
-	    '<img src="' + abs + 'images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'            + '\n' +
-	    '<img src="' + abs + 'images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">' + '\n' +
-	    '<img src="' + abs + 'images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'   + '\n' +
-	    '</p>'                                                                                           + '\n' +
-	    '</footer>'                                                                                      + '\n';
+	let footerCode =											  '\n' +
+	    '<footer>'												+ '\n' +
+	    '<p class="footer-text"> Website by Kat Cottrell | '						+ '\n' +
+	    
+	    '<a href="mailto:cottrellkat@gmail.com">'								+ '\n' +
+	    '<img src="' + abs + 'images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'		+ '\n' +
+	    '</a>'												+ '\n' +
+	    
+	    '<a href="tel:7274531850">'										+ '\n' +
+	    '<img src="' + abs + 'images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'		+ '\n' +
+	    '</a>'												+ '\n' +
+	    
+	    '<a href="https://www.linkedin.com/in/katherine-cottrell-656490168/">'				+ '\n' +
+	    '<img src="' + abs + 'images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">'	+ '\n' +
+	    '</a>'												+ '\n' +
+	    
+	    '<a href="https://github.com/katcottrell/">'							+ '\n' +
+	    '<img src="' + abs + 'images/githubFtIcon.png"   class="footer-icon" alt="Link to my GitHub">'	+ '\n' +
+	    '</a>'												+ '\n' +
+	    
+	    '</p>'												+ '\n' +
+	    '</footer>'												+ '\n';
 	document.write(footerCode);
 }
