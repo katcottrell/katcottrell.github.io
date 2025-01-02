af6c1b81a586fc40beae8d5b40e8a73261424ec3
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-01-02 15:11:57 -0500

    Create question.js

---
+++ js/question.js
@@ -0,0 +1,11 @@
+function makeQ(qImg, qText) {
+    let qHTML = 
+        '<table>\n' +
+        '<tr>\n' +
+        '<th><img src="' + qImg + '"></th>\n' +
+        '<th>' + qText + '</th>\n' +
+        '<th>Answer</th>\n' +
+        '</tr>\n' + 
+        '</table>';
+    document.write(headCode);
+}
