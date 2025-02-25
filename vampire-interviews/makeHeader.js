abb2432af50c91dd35a2e7993edffb5f75508356
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-02-25 10:14:35 -0500

    bottom line issue

--- vampire-interviews/makeHeader.js
+++ vampire-interviews/makeHeader.js
@@ -5,8 +5,8 @@ function makeHeader(tab) {
 	    '<header>'                                                                                                    + '\n' +
 	    '<link rel="stylesheet" href="https://katcottrell.github.io/vampire-interviews/nightStyle.css">'              + '\n' +
 	    '<div id="header-image style="border-bottom:solid 4px var(--myMidGray);">'                                    + '\n' +
-	    '<a href="https://katcottrell.github.io/vampire-interviews/nightlinks.html">'                                 + '\n' +
-	    '<img src="https://katcottrell.github.io/vampire-interviews/Nightlink%20banner.jpg" style="width:100%;"'      + '\n' +
+	    '<a href="https://katcottrell.github.io/vampire-interviews/nightlinks.html" style="line-height: 0px;padding: 0;margin: 0;float: left;">' + '\n' +
+	    '<img src="https://katcottrell.github.io/vampire-interviews/Nightlink%20banner.jpg" style="width:100%;border-bottom: solid 4px var(--myMidGray);"'      + '\n' +
 	    '</a>'                                                                                                        + '\n' +
 	    '</div>'                                                                                                      + '\n' +
 	    '</header>'                                                                                                   + '\n';
