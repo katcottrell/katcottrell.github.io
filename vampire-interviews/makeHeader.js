8c13f12194529ea093c389bd231750c8c63a7261
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-02-25 14:41:39 -0500

    Update makeHeader.js

--- vampire-interviews/makeHeader.js
+++ vampire-interviews/makeHeader.js
@@ -1,15 +1,12 @@
-function makeHeader(tab) {
-	let abs      = 'https://katcottrell.github.io/';
-	
+function makeHeader(tab) {	
 	let headerCode = 
 	    '<header>'                                                                                                    + '\n' +
 	    '<link rel="stylesheet" href="https://katcottrell.github.io/vampire-interviews/nightStyle.css">'              + '\n' +
-	    '<div id="header-image style="border-bottom:solid 4px var(--myMidGray);">'                                    + '\n' +
-	    '<a href="https://katcottrell.github.io/vampire-interviews/nightlinks.html" style="line-height: 0px;padding: 0;margin: 0;float: left;">' + '\n' +
-	    '<img src="https://katcottrell.github.io/vampire-interviews/Nightlink%20banner.jpg" style="width:100%;border-bottom: solid 4px var(--myMidGray);"'      + '\n' +
+	    '<div id="header-image" style="border-bottom:solid 4px var(--myMidGray);">'                                    + '\n' +
+	    '<a href="https://katcottrell.github.io/vampire-interviews/nightlinks.html" style="line-height:0px;padding:0;margin:0;float:left;">' + '\n' +
+	    '<img src="https://katcottrell.github.io/vampire-interviews/Nightlink%20banner.jpg" style="width:100%;border-bottom:solid 4px var(--myMidGray);">'      + '\n' +
 	    '</a>'                                                                                                        + '\n' +
 	    '</div>'                                                                                                      + '\n' +
 	    '</header>'                                                                                                   + '\n';
-	
 	document.write(headerCode);
 }
