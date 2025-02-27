bbd9356bb85d08fd9b4b31ccd6013dac77463c17
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-02-27 15:30:40 -0500

    Added hrs

--- vampire-interviews/makeSidenav.js
+++ vampire-interviews/makeSidenav.js
@@ -4,23 +4,35 @@ function makeSideNav(tab) {
 	let sideNavCode =                                                                                    '\n' +
                     '<div class="sidenav">'                                                                + '\n' +
                     '<a class="sideHeading" href="' + abs + 'nightlinks.html">Home</a>'                    + '\n' +
-                    '<hr style="width:90%;margin:10px 5%;">'                                               + '\n' +
+                    '<hr class="thickHr">'                                                                 + '\n' +
                     '<a class="sideHeading" href="' + abs + 'rules_faq.html">Rules & FAQ</a>'              + '\n' +
-                    '<hr style="width:90%;margin:10px 5%;">'                                               + '\n' +
+                    '<hr class="thickHr">'                                                                 + '\n' +
                     '<a class="sideHeading" href="' + abs + 'join.html">Join</a>'                          + '\n' +
-                    '<hr style="width:90%;margin:10px 5%;">'                                               + '\n' +
-                    '<a class="sideTabs" href="' + abs + 'all-listings.html">ALL LISTINGS</a>'             + '\n' +
+                    '<hr class="thickHr">'                                                                 + '\n' +
+                    '<a class="sideTabs" href="' + abs + 'all-listings.html">All Listings</a>'             + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'cosmetology.html">Cosmetology</a>'               + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'events-and-venues.html">Events &amp; Venues</a>' + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'media.html">Media</a>'                           + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'merchants.html">Merchants</a>'                   + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'music-and-audio.html">Music &amp; Audio</a>'     + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'occult.html">Occult</a>'                         + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'performing-arts.html">Performing Arts</a>'       + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'photo-and-video.html">Photo &amp; Video</a>'     + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'stem.html">STEM</a>'                             + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'visual-arts.html">Visual Arts</a>'               + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'writing.html">Writing</a>'                       + '\n' +
+                    '<hr class="skinnyHr">'                                                                + '\n' +
                     '<a class="sideTabs" href="' + abs + 'other.html">Other</a>'                           + '\n' +
                     '</div> <!-- END class="sidenav" -->'                                                  + '\n';
 	document.write(sideNavCode);
