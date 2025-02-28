025ac4414627ce327b9245f34888901c85f39613
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-02-28 13:40:00 -0500

    Update makeGallery.js

--- vampire-interviews/makeGallery.js
+++ vampire-interviews/makeGallery.js
@@ -1,17 +1,25 @@
 let abs = 'https://katcottrell.github.io/vampire-interviews/';
-const generic = ["default_pic.jpg", "https://www.w3schools.com/js/js_arrays.asp"]
+const generic = ["default_pic.jpg", "https://google.com",
+                 "default_pic.jpg", "https://google.com",
+                 "default_pic.jpg", "https://google.com",
+                 "default_pic.jpg", "https://google.com",
+                 "default_pic.jpg", "https://google.com"]
 
 function makeGallery(galList) {
-  let galleryCode = '\n';
-  for (let i = 0; i < galList.length; i = i + 6) { // Index of first image of each row in the gallery
-    galleryCode += '<div class="galleryRow" style="margin:auto;">\n';
-    for (let imgIdx = i; imgIdx < 6; imgIdx = imgIdx + 2) { // Index of filename of each image
-      linkIdx = imgIdx + 1;
-      galleryCode += '<a href="' + galList[linkIdx] + '">\n';
-      galleryCode += '<img src="' + abs + galList[imgIdx] + '">\n';
-      galleryCode += '</a>\n'
+    let galleryCode = '\n';
+    for (let rowOffset = 0; rowOffset < galList.length; rowOffset = rowOffset + 6) {
+        galleryCode += '<div class="galleryRow"">\n';
+        for (let fileNameOffset = 0; fileNameOffset < 6; fileNameOffset = fileNameOffset + 2) {
+            let fileNameIdx = parseInt(rowOffset) + parseInt(fileNameOffset);
+            let linkIdx = fileNameIdx + 1;
+            if (fileNameIdx < galList.length) {
+                galleryCode += '<a href="' + galList[linkIdx] + '">';
+                galleryCode += '<img class="thirds" src="' + abs + galList[fileNameIdx] + '" style="width:31%;margin:1%;">';
+                galleryCode += '</a>\n';
+            }
+        }
+        galleryCode += '</div> <!-- end galleryRow -->\n';
     }
-    galleryCode += '</div> <!-- end galleryRow -->\n';
-  }
-  document.write(galleryCode);
-}
+    document.write(galleryCode);
+} 
+makeGallery(generic);
