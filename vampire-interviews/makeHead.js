21aecbe743e82489719818a45558b27b16bc9cf6
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-03-02 16:41:53 -0500

    MOVED TO JS FOLDER

--- vampire-interviews/makeHead.js
+++ vampire-interviews/makeHead.js
@@ -1,8 +1,1 @@
-function makeHead(pageName) {	
-	let headCode =                                                                                            '\n' + 
-    '<head>'                                                                                              + '\n' + 
-    '<title>Nightlinks | ' + pageName + '</title>'                                                        + '\n' + 
-    '<link rel="icon" href="https://katcottrell.github.io/vampire-interviews/Nightlink%20-%20solid.png">' + '\n' + 
-    '</head>'                                                                                             + '\n';
-  document.write(headCode);
-}
+/* MOVED TO JS FOLDER */
