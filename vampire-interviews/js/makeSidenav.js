e448c9fdf2c76e590e4ae35748e0565661b8099d
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-03-02 23:00:00 -0500

    commented out calendar

--- vampire-interviews/js/makeSidenav.js
+++ vampire-interviews/js/makeSidenav.js
@@ -7,8 +7,8 @@ function makeSideNav(tab) {
                     '<hr class="thickHr">'                                                                            + '\n' +
                     '<a class="sideHeading" href="' + abs + 'about.html">About</a>'                                   + '\n' +
                     '<hr class="thickHr">'                                                                            + '\n' +
-                    '<a class="sideHeading" href="' + abs + 'calendar.html">Calendar</a>'                             + '\n' +
-                    '<hr class="thickHr">'                                                                            + '\n' +
+                 /* '<a class="sideHeading" href="' + abs + 'calendar.html">Calendar</a>'                             + '\n' +
+                    '<hr class="thickHr">'                                                                            + '\n' + */
                     '<a class="sideHeading" href="' + abs + 'join.html">Join</a>'                                     + '\n' +
                     '<hr class="thickHr">'                                                                            + '\n' +
                     '<a class="sideTabs" href="' + abs + 'categories/all.html">All Listings</a>'                      + '\n' +
