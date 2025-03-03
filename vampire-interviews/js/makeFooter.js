475b865502c3b21e62a2ba8c724f336b51f6c4e5
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-03-02 22:58:32 -0500

    updated img paths and ig link

--- vampire-interviews/js/makeFooter.js
+++ vampire-interviews/js/makeFooter.js
@@ -1,25 +1,25 @@
 function makeFooter() {
-	let abs = 'https://katcottrell.github.io/';
+	let absFoot = 'https://katcottrell.github.io/vampire-interviews/images/icons/';
 	let linkAttributes = 'target="_blank" rel="noopener noreferrer" style="color:transparent;"';
   
 	let footerCode =                                                                                      '\n' +
 	    '<footer>'                                                                                      + '\n' +
 	    '<p class="footer-text"> Website by Kat | '                                                     + '\n' +
 	    
-	    '<a href="google.com" ' + linkAttributes + '>'                                                  + '\n' +
-	    '<img src="' + abs + 'vampire-interviews/ig_icon.png"   class="footer-icon" alt="Instagram">'   + '\n' +
+	    '<a href="https://www.instagram.com/nightlinks_nyc" ' + linkAttributes + '>'                    + '\n' +
+	    '<img src="' + absFoot + 'ig_icon.png" class="footer-icon" alt="Instagram">'                    + '\n' +
 	    '</a>'                                                                                          + '\n' +
 	    
 	    '<a href="https://github.com/katcottrell/" ' + linkAttributes + '>'                             + '\n' +
-	    '<img src="' + abs + 'vampire-interviews/fb_icon.png"   class="footer-icon" alt="Facebook">'    + '\n' +
+	    '<img src="' + absFoot + 'fb_icon.png" class="footer-icon" alt="Facebook">'                     + '\n' +
 	    '</a>'                                                                                          + '\n' +
 	    
 	    '<a href="mailto:fledermischief@gmail.com" ' + linkAttributes + '>'                             + '\n' + 
-	    '<img src="' + abs + 'vampire-interviews/discord_icon.png" class="footer-icon" alt="Discord">'  + '\n' +
+	    '<img src="' + absFoot + 'discord_icon.png" class="footer-icon" alt="Discord">'                 + '\n' +
 	    '</a>'                                                                                          + '\n' +
 	    
 	    '<a href="mailto:fledermischief@gmail.com" ' + linkAttributes + '>'                             + '\n' + 
-	    '<img src="' + abs + 'vampire-interviews/mail_icon.png"     class="footer-icon" alt="Email">'   + '\n' +
+	    '<img src="' + absFoot + 'mail_icon.png" class="footer-icon" alt="Email">'                      + '\n' +
 	    '</a>'                                                                                          + '\n' +
 	    
 	    '</p>'                                                                                          + '\n' +
