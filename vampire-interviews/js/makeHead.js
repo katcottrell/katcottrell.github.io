cac80ba7c73305b6f13c6d00c12345d64db56860
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2025-03-02 23:54:03 -0500

    img path

--- vampire-interviews/js/makeHead.js
+++ vampire-interviews/js/makeHead.js
@@ -2,7 +2,7 @@ function makeHead(pageName) {
 	let headCode =                                                                                            '\n' + 
     '<head>'                                                                                                     + '\n' + 
     '<title>Nightlinks | ' + pageName + '</title>'                                                               + '\n' + 
-    '<link rel="icon" href="https://katcottrell.github.io/vampire-interviews/images/Nightlink%20-%20solid.png">' + '\n' + 
+    '<link rel="icon" href="https://katcottrell.github.io/vampire-interviews/images/icons/Nightlink%20-%20solid.png">' + '\n' + 
     '</head>'                                                                                                    + '\n';
   document.write(headCode);
 }
