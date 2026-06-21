6623e906bcd9657dceb6746339f0c1f18dde2cb0
Kat Cottrell <98633221+katcottrell@users.noreply.github.com>
2026-06-21 14:58:59 -0400

    Removed phone number from footer

--- js/footer.js
+++ js/footer.js
@@ -9,10 +9,6 @@ function makeFooter() {
 	    '<img src="' + abs + 'images/mailFtIcon.png"     class="footer-icon" alt="Email me!">'		+ '\n' +
 	    '</a>'												+ '\n' +
 	    
-	    '<a href="tel:7274531850" ' + linkAttributes + '>'							+ '\n' +
-	    '<img src="' + abs + 'images/phoneFtIcon.png"    class="footer-icon" alt="Call me!">'		+ '\n' +
-	    '</a>'												+ '\n' +
-	    
 	    '<a href="https://www.linkedin.com/in/katherine-cottrell-656490168/" ' + linkAttributes + '>'	+ '\n' +
 	    '<img src="' + abs + 'images/linkedinFtIcon.png" class="footer-icon" alt="Link to my LinkedIn">'	+ '\n' +
 	    '</a>'												+ '\n' +
